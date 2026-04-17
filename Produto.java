fd903932a8ad7663fb9374189bf12a8727932298
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-04-16 21:46:39 -0300

    [feat] - Cria o construtor da classe produto

--- Produto.java
+++ Produto.java
@@ -5,4 +5,11 @@ public class Produto {
     private double preco;
     private int quantidade;
 
+    public Produto(String nome, String categoria, double preco, int quantidade) {
+
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
 }
